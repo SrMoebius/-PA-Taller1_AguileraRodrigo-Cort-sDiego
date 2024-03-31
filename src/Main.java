58b734ef68f373bf0a4325811be4e1dfab568fea
SrMoebius <164018015+SrMoebius@users.noreply.github.com>
2024-03-31 03:49:50 -0300

    Prueba 1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import ucn.*;
 public class Main {
     public static void main(String[] args) {
 
-        StdOut.println("Hola Mundo!");
+        StdOut.println("Hola Mundillo de youtube!");
+        StdOut.println("UwU");
+
+        StdOut.println("\nPrueba 1");
 
     }
 }
