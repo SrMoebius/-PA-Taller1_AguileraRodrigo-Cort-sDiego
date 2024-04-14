5a86d0a99c2fb29fc6cccd5d49f3b966c7a565e1
DiegoCortes07 <diego.cortes07@alumnos.ucn.cl>
2024-04-13 22:36:48 -0400

    new menu

--- src/Programa/Main.java
+++ src/Programa/Main.java
@@ -10,5 +10,48 @@ public class Main {
 
         StdOut.println("\nPrueba 1");
 
+       menu();
+    }
+
+
+    public static void menu(){
+
+        Boolean menuActivado = true;
+
+        while (!menuActivado != false){
+
+
+            StdOut.println("***********************************************");
+            StdOut.println("Bienvenido al sistema de ventas de videojuegos ");
+            StdOut.println("***********************************************");
+            StdOut.println("Ingrese una opcion para continuar: ");
+            StdOut.println("1) Ingresar Sesion ");
+            StdOut.println("2) Cerrar Programa");
+
+            StdOut.print("opcion:");
+            String opcion = StdIn.readString();
+
+            switch (opcion){
+
+
+                case "1" -> {
+
+                }
+
+
+                case "2"-> {
+
+
+                }
+
+
+
+                default -> {
+
+                    StdOut.println("El termino "+opcion+" no es una opcion valida");
+                }
+
+            }
+        }
     }
 }
